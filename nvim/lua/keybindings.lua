460732801ba9c4a8b6bc9b06b11aa5972bb28dbe
qxchen <qxchencoding@outlook.com>
2023-08-05 21:33:13 +0800

    added COC keybindings

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,13 +1,20 @@
+-- Fundamentals
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 local pluginKeys = {}
 local map = vim.api.nvim_set_keymap
 local opt = { noremap = true, silent = true }
 local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
-
+-- Editors
 map("n", "<C-q>", ":q<CR>", opt)
 map("n", "<C-s>", ":w<CR>", opt)
-map("n", "<Space>", [[@=((foldclosed(line('.')) < 0) ? 'zc' : 'zo')<CR>]], { noremap = true, silent = true })
+map("n", "<C-j>", "5j", opt)
+map("n", "<C-k>", "5k", opt)
+-- Move selected words
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+-- Fold/unfold
+map("n", "<Space>", [[@=((foldclosed(line('.')) < 0) ? 'zc' : 'zo')<CR>]], opt)
 -- Press i to enter insert mode, and ii to exit insert mode.
 -- And so on
 map("i", "ii", "<Esc>", opt)
@@ -21,18 +28,40 @@ map("x", "dd", '"_dd', opt)
 map("n", "dw", '"_dw', opt)
 map("x", "dw", '"_dw', opt)
 
--- COC Intellisence
+-- COC
 function _G.check_back_space()
 	local col = vim.fn.col(".") - 1
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
 end
-map("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<C-g>u<Tab>"]], { expr = true, noremap = true, silent = true })
-map("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<C-h>"]], { expr = true, noremap = true })
+map("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<C-g>u<Tab>"]], opts)
+map("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<C-h>"]], opts)
 -- map("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 -- map("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+-- Make <CR> to accept selected completion item or notify coc.nvim to format
+-- <C-g>u breaks current undo, please make your own choice
 map("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
--- COC
-map("n", vim.g.mapleader .. "rn", "<Plug>(coc-rename)", {})
+-- Use <c-j> to trigger snippets
+map("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)", { silent = true, expr = true })
+-- GoTo code navigation
+map("n", "gd", "<Plug>(coc-definition)", { silent = true })
+map("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
+map("n", "gi", "<Plug>(coc-implementation)", { silent = true })
+map("n", "gr", "<Plug>(coc-references)", { silent = true })
+-- Use K to show documentation in preview window
+function _G.show_docs()
+	local cw = vim.fn.expand("<cword>")
+	if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command("h " .. cw)
+	elseif vim.api.nvim_eval("coc#rpc#ready()") then
+		vim.fn.CocActionAsync("doHover")
+	else
+		vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
+	end
+end
+map("n", "K", "<CMD>lua _G.show_docs()<CR>", { silent = true })
+-- Symbol renaming
+map("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
 
 -- Formatter
 map("n", "<leader>f", ":Format<CR>", opt)
