40e322a8d844328700201901067f161e58e1b172
qxchen <qxchencoding@outlook.com>
2023-08-05 17:29:25 +0800

    deleted old mixed configs

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,77 +1,73 @@
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
-
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+local pluginKeys = {}
 local map = vim.api.nvim_set_keymap
-local opt = {noremap = true, silent = true}
+local opt = { noremap = true, silent = true }
+local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
 
-map('n', '<C-q>', ':q<CR>', opt)
-map('n', '<C-s>', ':w<CR>', opt)
+map("n", "<C-q>", ":q<CR>", opt)
+map("n", "<C-s>", ":w<CR>", opt)
+map("n", "<Space>", [[@=((foldclosed(line('.')) < 0) ? 'zc' : 'zo')<CR>]], { noremap = true, silent = true })
+-- Press i to enter insert mode, and ii to exit insert mode.
+-- And so on
+map("i", "ii", "<Esc>", opt)
+map("i", "jk", "<Esc>", opt)
+map("i", "kj", "<Esc>", opt)
+map("v", "jk", "<Esc>", opt)
+map("v", "kj", "<Esc>", opt)
 
---- split windows
-map('n', 's', '', opt)
-map('n', 'sv', ':vsp<CR>', opt)
-map('n', 'sh', ':sp<CR>', opt)
---- resize windows
---map("n", "<C-l>", ":vertical resize -2<CR>", opt)
---map("n", "<C-h>", ":vertical resize +2<CR>", opt)
---map("n", "<C-k>", ":resize +2<CR>", opt)
---map("n", "<C-j>", ":resize -2<CR>", opt)
+map("n", "dd", '"_dd', opt)
+map("x", "dd", '"_dd', opt)
+map("n", "dw", '"_dw', opt)
+map("x", "dw", '"_dw', opt)
 
-local pluginkeys = {}
+-- COC Intellisence
+function _G.check_back_space()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+end
+map("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<C-g>u<Tab>"]], { expr = true, noremap = true, silent = true })
+map("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<C-h>"]], { expr = true, noremap = true })
+-- map("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+-- map("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+map("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+-- COC
+map("n", vim.g.mapleader .. "rn", "<Plug>(coc-rename)", {})
 
---- nvim-tree
-map('n', '<S-q>', ':NvimTreeToggle<CR>', opt)
-pluginkeys.nvimTreeList = {
-    {key = {'<CR>', 'o', '<2-LeftMouse>'}, action = 'edit'},
-    {key = 'v', action = 'vsplit'},
-    {key = 'h', action = 'split'},
-    {key = '.', action = 'toggle_dotfiles'},
-    {key = '<leader>rn', action = 'rename'},
-    {key = 'r', action = 'refresh'},
-    {key = '<leader>a', action = 'create'},
-    {key = '<leader>d', action = 'remove'}
+-- Formatter
+map("n", "<leader>f", ":Format<CR>", opt)
+map("n", "<leader>F", ":FormatWrite<CR>", opt)
+
+-- NvimTree
+map("n", "<A-m>", ":NvimTreeToggle<CR>", opt)
+pluginKeys.nvimTreeList = {
+	-- 打开文件或文件夹
+	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+	-- 分屏打开文件
+	{ key = "v", action = "vsplit" },
+	{ key = "h", action = "split" },
+	-- 显示隐藏文件
+	{ key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
+	{ key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
+	-- 文件操作
+	{ key = "<F5>", action = "refresh" },
+	{ key = "a", action = "create" },
+	{ key = "d", action = "remove" },
+	{ key = "r", action = "rename" },
+	{ key = "x", action = "cut" },
+	{ key = "c", action = "copy" },
+	{ key = "p", action = "paste" },
+	{ key = "s", action = "system_open" },
 }
 
 --- bufferline
---- switch between tabs
+--- Switch between tabs
 map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
 map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
-
 --- close tabs
-map("n", "<C-w>w", ":Bdelete!<CR>", opt)     --- close
+map("n", "<C-w>w", ":Bdelete!<CR>", opt)
 --map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)      --- close the right tab
 --map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)     --- close the left tab
 --map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)     --- close the selected tab
 
---- telescope
-map("n", "<C-p>", ":Telescope find_files<CR>", opt)     --- search files
-map("n", "<C-f>", ":Telescope live_grep<CR>", opt)    --- global search
-pluginkeys.telescopeList = {
-  i = {
-    -- move vertically
-    ["<C-j>"] = "move_selection_next",
-    ["<C-k>"] = "move_selection_previous",
-    ["<Down>"] = "move_selection_next",
-    ["<Up>"] = "move_selection_previous",
-    -- history
-    ["<C-n>"] = "cycle_history_next",
-    ["<C-p>"] = "cycle_history_prev",
-    -- close the window
-    ["<C-c>"] = "close",
-    -- sroll the preview window
-    ["<C-u>"] = "preview_scrolling_up",
-    ["<C-d>"] = "preview_scrolling_down",
-  },
-}
-
---- Flutter
-map("n", "<leader>fe", ":CocCommand flutter.emulators<CR>", opt)
-map("n", "<leader>fd", ":below new output:///flutter-dev<CR>", opt)
-map("n", "<leader>fr", ":CocCommand flutter.run<CR>", opt)
-
---- Markdown
-vim.g.nvim_markdown_preview_format = 'markdown'
-map("n", "<leader>md", ":MarkdownPreview<CR>", opt)
-
-
-return pluginkeys
+return pluginKeys
--- nvim/plugins/floaterm.vim
+++
@@ -1,5 +0,0 @@
-let g:floaterm_keymap_new = '<leader>t'
-let g:floaterm_keymap_toggle = '<leader>tt'
-let g:floaterm_keymap_kill = '<leader>kt'
-let g:floaterm_width = 0.8
-let g:floaterm_height = 0.9
